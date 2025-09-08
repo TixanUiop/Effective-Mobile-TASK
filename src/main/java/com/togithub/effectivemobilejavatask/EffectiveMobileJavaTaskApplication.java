ff95447c1a71122b3943760fe04269f2845fb53d
Evgeny Loshonkov <tixanuiop@gmail.com>
2025-09-08 21:18:37 +0300

    created base structure

---
+++ src/main/java/com/togithub/effectivemobilejavatask/EffectiveMobileJavaTaskApplication.java
@@ -0,0 +1,13 @@
+package com.togithub.effectivemobilejavatask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EffectiveMobileJavaTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EffectiveMobileJavaTaskApplication.class, args);
+    }
+
+}
