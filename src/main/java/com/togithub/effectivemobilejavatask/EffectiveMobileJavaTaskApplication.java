28683f4d79480832d46a94a344eb1ab518bbc87e
Evgeny Loshonkov <tixanuiop@gmail.com>
2025-09-10 12:39:37 +0300

    fix

--- src/main/java/com/togithub/effectivemobilejavatask/EffectiveMobileJavaTaskApplication.java
+++ src/main/java/com/togithub/effectivemobilejavatask/EffectiveMobileJavaTaskApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 
-// https://github.com/PaatoM/Bank_REST
-// https://forms.yandex.ru/u/68220273e010db1881d1132d/
 @SpringBootApplication
 @EnableMethodSecurity(prePostEnabled = true)
 public class EffectiveMobileJavaTaskApplication {
